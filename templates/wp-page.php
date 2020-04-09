8e226a18306d2b2f85404716c203ca98b789031b
Mathilde Arconte <signup@marconte.fr>
2020-04-09 13:07:55 +0200

    maj section equipe

--- templates/wp-page.php
+++ templates/wp-page.php
@@ -10,15 +10,12 @@
 <div class="container">
 
 <main id="page">
-
-  <? if (has_post_thumbnail()) : ?>
-    <section>
-      <div class="element teaser">
-        <? the_post_thumbnail('large', ['class' => 'modernizr-of']); ?>
-      </div>
+<? if (has_post_thumbnail()) : ?>
+    <section class="page__header">
+    <h1 class="page__header__title text-center"><? the_title(); ?></h1>
+        <? the_post_thumbnail('large'); ?>
     </section>
   <? endif?>
-
   <section>
     <? while (have_posts()) : the_post(); ?>
       <article>
